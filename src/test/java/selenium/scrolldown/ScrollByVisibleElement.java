540405d6854cbecc4405ab40ebfeb408772b9f6e
Ganesh Jadhav <Ganesh.Jadhav@emtecinc.com>
2022-05-02 08:57:36 +0530

    GJ/NEWCodeCommitted

--- src/test/java/selenium/scrolldown/ScrollByVisibleElement.java
+++ src/test/java/selenium/scrolldown/ScrollByVisibleElement.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class ScrollByVisibleElement {
     @Test
     public void verifyScrollByVisibleElement() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ganesh.jadhav\\Repo\\SeleniumProject2.0\\src\\test\\java\\selenium\\browser\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://selenium08.blogspot.com/2020/01/drag-drop.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.manage().window().maximize();
         WebElement srcElement = driver.findElement(By.id("draggable"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
